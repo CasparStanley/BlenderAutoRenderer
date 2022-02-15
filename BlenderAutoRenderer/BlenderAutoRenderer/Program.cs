ff0b767fed3657c7629a44707ab79170f39f7f42
Caspar Stanley <casparstan@gmail.com>
2022-02-16 00:39:19 +0100

    Setup and start of code
    Let's do this!

---
+++ BlenderAutoRenderer/BlenderAutoRenderer/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace BlenderAutoRenderer
+{
+    class Program
+    {
+        // Render Options
+        const string Opt_Animation = "-a"; // Run in background (often used for UI-less rendering).
+        const string Opt_RunInBackground = "-b"; // Render frames from start to end (inclusive).
+        const string Opt_SceneName = "-S"; // Set the active scene <name> for rendering.
+        const string Opt_RenderFrame = "-f"; // Render frame <frame> and save it.
+
+        // Render Settings
+
+
+        // Animation Settings
+        private bool Animation = false;
+        private int FrameStart = 1;
+        private int FrameEnd = 250;
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! Welcome to the Blender Auto Renderer\nMade by Caspar Stanley, 2022");
+            Start();
+        }
+
+        static void Start()
+        {
+
+
+            Run();
+        }
+
+        static void Run()
+        {
+
+        }
+    }
+}
